a12833ef457015880c10a38777cbbe84e488f6b7
QuesitoSucio <mantillatorresduvanandres@gmail.com>
2025-10-02 16:59:17 -0500

    cambio1

---
+++ controllers/controllerAttendant.js
@@ -0,0 +1,9 @@
+/* Acudientes: (rector, coordinador)Estos dos roles solo podrán listar (secretaria)Este rol es el único que podrá hacer todas las peticiones
+•	GET /api/acudientes - Listar todos los acudientes 
+•	GET /api/acudientes/buscar - Buscar por documento/nombre
+•	GET /api/acudientes/:id - Obtener acudientes por ID
+•	POST /api/acudientes -  crear acudientes 
+•	PUT /api/acudientes/:id -  actualizar acudientes por ID 
+•	PUT /api/acudientes/:id/activar -  activar acudiente
+•	PUT /api/acudientes/:id/desactivar -  desactivar acudiente
+•	DELETE /api/acudientes/:id - eliminar acudiente */
---
+++ models/attendant.js
@@ -0,0 +1,69 @@
+/* 11. Acudiente  (Rector, Coordinador y Secretaria)
+{
+_id: Object
+nombres: String,
+apellidos: String,
+telefono: String,
+documento: String,
+tipo de documento: String,
+email: String,
+password: String,
+isActive: Boolean,
+}, */
+import mongoose, { model } from 'mongoose';
+
+const mongoose = require('mongoose');
+const AcudienteSchema = new mongoose.Schema({
+
+    name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  surnames: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: false,
+    trim: true
+  },
+  document: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  documenttype: {
+    type: String,
+    required: true,
+    enum: ['CC', 'TI', 'CE', 'Otro'], 
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  rol: {
+    type: String,
+    enum: ['Rector', 'Coordinador', 'Secretaria'],
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Acudiente', AcudienteSchema);
